56aedbf7c1a4ee07521019907074cbea8a21e2e9
mattgefen <matt.gefen@gmail.com>
2021-12-07 11:44:03 -0700

    done!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Procedures from './components/Procedures';
+import Contact from './components/Contact'
 
 import './styles.css';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+
+ReactDOM.render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />}>
+      <Route path='procedures' element={<Procedures />}/>
+      <Route path='contact' element={<Contact />}/>
+    </Route>
+  </Routes>
+  </BrowserRouter>, 
+  rootElement
+);
